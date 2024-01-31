b0c8239659af28855daa1865075483a98fabb0f3
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2024-01-30 23:49:07 -0300

    commit

---
+++ Models/DevameetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DEVAMEET_CSharp.Models
+{
+    public class DevameetContext : DbContext
+    {
+        public DevameetContext(DbContextOptions<DevameetContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DEVAMEET_CSharp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectstring = builder.Configuration.GetConnectionString("DefaulConnectString");
+builder.Services.AddDbContext<DevameetContext>(option => option.UseSqlServer(connectstring));
 
 var app = builder.Build();
 
